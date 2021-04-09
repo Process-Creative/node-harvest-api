c5568abc6d1207325c2176b23abe612a5bba9e32
Dominic Masters <dominic@domsplace.com>
2021-04-09 17:27:01 +1000

    Added deploy to regular NPM

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,5 @@
+npm publish
+
 npm config set "@process-creative:registry" "https://npm.pkg.github.com/" --global
 echo "registry=https://npm.pkg.github.com/" > ".npmrc"
 echo "//npm.pkg.github.com/:_authToken=${GH_TOKEN}" > ".npmrc"
